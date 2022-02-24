4c5b9c22a69549b032208fd97f09bde2eea78d42
Jennifer <jenniferlewmunoz@gmail.com>
2022-02-24 12:34:07 -0800

    Added personal assets and meter

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 /***********************************************************************************
   ClickableAllocator
   by Scott Kildall
+  student Jennifer Lew Munoz
   
   Start your localhost before running this, otherwise no PNGs will display
 
@@ -37,20 +38,46 @@ const minDeflateDiameter = 5;
 // variables for the ballon
 var ellipseDiameter = startEllipseDiameter;
 
-// pop soun
+// pop sound
 var popSound;
+
+// constants for array of squares
+const meterMaxIndex = 9;
+var meter = [];
+var meterIndex = 0;
+
+// food images
+let eLiquid;
+let pLiquid;
+let eCold;
+let pCold;
+let eHot;
+let pHot;
+
+// array of text options
+var textOptions =
+  ["Edible liquid pod, or plastic water bottle?",
+    "Edible popsicle wrapper, or plastic popsicle wrapper?",
+    "Veggie soup in celery flavored container, or plastic container?"];
+var textIndex = 0;
 
 // ALWAYS allocate the ClickableManager in the preload() function
 // if you get an error here, it is likely the .csv file that is not the
 // correct filename or path
-function preload(){
+function preload() {
   clickablesManager = new ClickableManager('assets/clickableLayout.csv');
+  eLiquid = loadImage('assets/edible_liquid.png');
+  pLiquid = loadImage('assets/plastic_liquid.png');
+  eCold = loadImage('assets/edible_cold.png');
+  pCold = loadImage('assets/plastic_cold.png');
+  eHot = loadImage('assets/edible_hot.png');
+  pHot = loadImage('assets/plastic_hot.png');
 }
 
 // ALWAYS call the setup() funciton for ClickableManager in the setup(), after
 // the class has been allocated in the preload() function.
 function setup() {
-  createCanvas(1280,600);
+  createCanvas(600, 600);
 
   // load the pop sound
   soundFormats('mp3');
@@ -61,33 +88,65 @@ function setup() {
 
   // call OUR function to setup additional information about the p5.clickables
   // that are not in the array 
-  setupClickables(); 
+  setupClickables();
 
   // start with a red balloon
   newBalloon(redIndex);
 
   // output to the message window
   console.log(clickables);
+
+  // set all squares to false
+  setupMeter();
 }
 
 // Just draw the button
 function draw() {
   background(128);
 
-  // draw "balloon"
-  drawBalloon();
+  // draw "meter"
+  drawMeter();
 
   // draw the p5.clickables
   clickablesManager.draw();
+
 }
 
-function drawBalloon() {
+function setupMeter() {
+  for (let i = 0; i <= meterMaxIndex; i++) {
+    meter[i] = false;
+  }
+}
+
+function drawMeter() {
   push();
-  ellipseMode(CENTER);
+
+  text(textOptions[textIndex], 100, 450);
+
+  if (textIndex == 0) {
+    image(eLiquid, 25, 200);
+    image(pLiquid, 250, 200);
+  } else if (textIndex == 1) {
+    image(eCold, 25, 200);
+    image(pCold, 250, 200);
+  } else if (textIndex == 2) {
+    image(eHot, 25, 200);
+    image(pHot, 250, 200);
+  }
+
+  text("DANGER METER", 480, 450);
   noStroke();
-  fill(balloonColor);
-  circle(250,height/2, ellipseDiameter);
+  fill(255, 20, 0);
+  for (let i = 0; i <= meterMaxIndex; i++) {
+    if (meter[i]) {
+      square(515, 400 - (30 * i), 30);
+    }
+  }
   pop();
+}
+
+function drawLiquidText() {
+  text("Edible water pod, or plastic water bottle?");
 }
 
 // change individual fields of the clickables
@@ -95,10 +154,10 @@ function setupClickables() {
   // set the pop, inflate and deflate to be false, we will change this after
   // first balloon gets pressed
   clickables[inflateIndex].visible = false;
-  clickables[deflateIndex].visible = false; 
+  clickables[deflateIndex].visible = false;
 
   // These are the CALLBACK functions. Right now, we do the SAME function for all of the clickables
-  for( let i = 0; i < clickables.length; i++ ) {
+  for (let i = 0; i < clickables.length; i++) {
     clickables[i].onPress = clickableButtonPressed;
     clickables[i].onHover = clickableButtonHover;
     clickables[i].onOutside = clickableButtonOnOutside;
@@ -108,35 +167,45 @@ function setupClickables() {
 //--- CLICKABLE CALLBACK FUNCTIONS ----
 
 clickableButtonPressed = function () {
-// NEW BALLOON
-  if( this.id === redIndex || this.id === greenIndex || this.id === yellowIndex ) {
+  // Call newBalloon()
+  if (this.id === yellowIndex || this.id === greenIndex) {
     newBalloon(this.id);
   }
-
-// INFLATE OR DEFLATE
-  else if( this.id === deflateIndex ) {
-    ellipseDiameter -= deflateAmount;
-    ellipseDiameter = max(minDeflateDiameter,ellipseDiameter);   // prevents < 0
+  // Make user decide for liquid food
+  else if (this.id === redIndex) {
+    drawLiquidText();
   }
-  else if( this.id === inflateIndex ) {
-    ellipseDiameter += inflateAmount;
-    if( ellipseDiameter > maxDiameter ) {
-      popBalloon();
+
+  // Decrement the danger meter
+  else if (this.id === deflateIndex) {
+
+    if (meterIndex > 0) {
+      meter[meterIndex] = false;
+      meterIndex = meterIndex - 1;
+    }
+  }
+
+  // Increase the danger meter
+  else if (this.id === inflateIndex) {
+
+    if (meterIndex < meterMaxIndex) {
+      meter[meterIndex] = true;
+      meterIndex = meterIndex + 1;
     }
   }
 }
 
 // tint when mouse is over
 clickableButtonHover = function () {
-  this.color = "#AA33AA";
+  this.color = "#add8e6";
   this.noTint = false;
-  this.tint = "#FF0000";
+  this.tint = "#add8e6";
 }
 
 // color a light gray if off
 clickableButtonOnOutside = function () {
   // Change colors based on the id #
-  if( this.id === inflateIndex || this.id === deflateIndex ) {
+  if (this.id === inflateIndex || this.id === deflateIndex) {
     this.color = "#FFFFFF";
   }
   else {
@@ -152,18 +221,18 @@ clickableButtonOnOutside = function () {
 // change fill color and reset ellipse diamter
 function newBalloon(idNum) {
   clickables[inflateIndex].visible = true;
-  clickables[deflateIndex].visible = true; 
-  ellipseDiameter = startEllipseDiameter;
+  clickables[deflateIndex].visible = true;
 
-  if( idNum === redIndex) {
-    balloonColor = color('#FF0000');
+  if (idNum === redIndex) {
+    textIndex = 0;
   }
-  else if( idNum === greenIndex) {
-    balloonColor = color('#00FF00');
+  else if (idNum === greenIndex) {
+    textIndex = 1;
   }
-  else if( idNum === yellowIndex) {
-    balloonColor = color('#FFFF00');
+  else if (idNum === yellowIndex) {
+    textIndex = 2;
   }
+
 }
 
 // if we pop the balloon, then you can't re-pop or inflate or deflate
@@ -174,7 +243,7 @@ function popBalloon() {
 
   // balloon popped, hide these buttons
   clickables[inflateIndex].visible = false;
-  clickables[deflateIndex].visible = false; 
+  clickables[deflateIndex].visible = false;
 }
 
 
